582783bd2587ce8dd0c7daaece4d109d1b2ddfba
Pawel <frubelek@gmail.com>
2021-01-16 10:29:12 +0000

    [init] - initial project commit

---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,8 @@
+// root package = groupId + artifactId
+package pl.sda.javalondek4.java_demo;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Witajmy na zajęciach u Mańka :)");
+    }
+}
