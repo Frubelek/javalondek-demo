802667d59f662b299cf10d7987e6d9fb8b1855fe
Pawel <frubelek@gmail.com>
2021-01-17 13:36:26 +0000

    [change] - calculator with four base operations completed

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/CalcDemo.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/CalcDemo.java
@@ -13,8 +13,29 @@ public class CalcDemo {
             return first + second;
         };
 
+//        IDEAŁ \/
+        Adder adder3 = (first, second) -> first + second;
+//        IDEAŁ /\
+
         StrangeOne strangeOne = (int a, int b) -> {
             return a + b;
         };
+
+        StringAdder stringAdder = ((one, two) -> {
+            return one.concat(two);
+        });
+
+        MyAdder myAdder = (a, b, comment) -> {
+            System.out.println(comment);
+            return a + b;
+        };
+
+        Substraction substraction = (a, b) -> a - b;
+        Multiplication multiplication = (a, b) -> a * b;
+        Division division = (a, b) -> a / b;
+
+        myAdder.addWithComment(5, 20, "should be 25");
+
+
     }
 }
